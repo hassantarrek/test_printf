8c28b6a9bb922e7d5afece66c4cd605ceedd3a46
hassantarrek <htarek168@gmail.com>
2023-08-19 06:52:51 -0700

    editing main.h

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
  * header file for printf functions
  * adding decelration of printf functions
  */
 
 int _printf(const char *format, ...);
+
+#endif
